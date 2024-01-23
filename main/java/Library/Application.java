69987aca8f1378e2ed3fb7c041f19574cdb4f4fe
Irek <general157@mail.ru>
2024-01-23 18:31:08 +0300

    library application added

---
+++ main/java/Library/Application.java
@@ -0,0 +1,11 @@
+package Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Library.Application.class, args);
+    }
+}
