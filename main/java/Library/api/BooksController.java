985b4c29ea12f7923b064be3473800e81cbae9c5
Irek <general157@mail.ru>
2024-02-07 15:43:54 +0300

    BooksController updated with Swagger annotations

--- main/java/Library/api/BooksController.java
+++ main/java/Library/api/BooksController.java
@@ -1,6 +1,10 @@
 package Library.api;
 
 import Library.model.Book;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +14,7 @@ import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/book")
+@Tag(name = "Book")
 public class BooksController {
     private final BookService bookService;
 
@@ -18,6 +23,10 @@ public class BooksController {
     }
 
     @GetMapping
+    @Operation(summary = "get all books", description = "Возвращает все доступные книги в библиотеке")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "404", description = "not found") })
     public ResponseEntity<List<Book>> getAll() {
         try {
             List<Book> books = bookService.getAll();
@@ -28,6 +37,11 @@ public class BooksController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "get book by ID", description = "Возвращает книгу по заданному идентификатору")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "404", description = "book not found")
+    })
     public ResponseEntity<Book> getBook(@PathVariable Integer id) {
         try {
             Book book = bookService.getById(id);
@@ -38,6 +52,11 @@ public class BooksController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "delete book by ID", description = "Удаляет книгу по заданному идентификатору")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "404", description = "book not found")
+    })
     public ResponseEntity<Object> deleteById(@PathVariable Integer id) {
         try {
             bookService.deleteById(id);
@@ -48,6 +67,11 @@ public class BooksController {
     }
 
     @PostMapping
+    @Operation(summary = "add new book", description = "Добавляет книгу")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "404", description = "book not found")
+    })
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
         Book savedBook = bookService.addBook(book);
         return ResponseEntity.ok(savedBook);
