aebf6e678b1c2f3f8be1080df8dbd84b74cf46a4
Irek <general157@mail.ru>
2024-02-12 19:04:44 +0300

    SecurityConfig added

---
+++ main/java/Library/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package Library.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/ui/**").authenticated()
+                        .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
