6d82a91cae7c74d682306f646a88f4b24539fa57
Irek <general157@mail.ru>
2024-02-12 20:10:36 +0300

    UserRepository added

---
+++ main/java/Library/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package Library.repository;
+
+import Library.model.Book;
+import Library.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    public Optional<User> findByLogin(String login);
+}
